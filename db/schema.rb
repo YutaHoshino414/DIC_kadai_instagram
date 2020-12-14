8877f716cd95b81e4d3b4f036a25e438e3fcb46a
Yuta Hoshino <y.hoshino25@gmail.com>
2020-12-14 18:45:09 +0900

    userテーブル/画像uploader生成

--- db/schema.rb
+++ db/schema.rb
@@ -10,40 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_12_13_150542) do
+ActiveRecord::Schema.define(version: 2020_12_14_091858) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
-
-  create_table "pictures", force: :cascade do |t|
-    t.string "title"
-    t.text "image"
-    t.bigint "user_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["user_id"], name: "index_pictures_on_user_id"
-  end
-
-  create_table "user_profiles", force: :cascade do |t|
-    t.text "user_profile"
-    t.text "user_image"
-    t.bigint "user_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["user_id"], name: "index_user_profiles_on_user_id"
-  end
 
   create_table "users", force: :cascade do |t|
     t.string "name"
     t.string "email"
     t.string "password_digest"
-    t.text "profile"
-    t.text "image"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index ["email"], name: "index_users_on_email", unique: true
   end
 
-  add_foreign_key "pictures", "users"
-  add_foreign_key "user_profiles", "users"
 end
