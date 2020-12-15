c88a30925e610808dccf789b4ebc3e6fed9cc05e
Yuta Hoshino <y.hoshino25@gmail.com>
2020-12-15 19:14:30 +0900

    emailにインデックス追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_12_15_100156) do
+ActiveRecord::Schema.define(version: 2020_12_15_100935) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -31,6 +31,7 @@ ActiveRecord::Schema.define(version: 2020_12_15_100156) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.text "image"
+    t.index ["email"], name: "index_users_on_email", unique: true
   end
 
   add_foreign_key "pictures", "users"
