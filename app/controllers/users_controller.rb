a2c735fd27962308fe033909b57003cfaff8d3f4
Yuta Hoshino <y.hoshino25@gmail.com>
2020-12-15 13:56:39 +0900

    user情報編集機能実装

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,6 +6,7 @@ class UsersController < ApplicationController
 
   def create
     @user = User.new(user_params)
+    # @user.image = "default.jpg"
     if @user.save
       redirect_to user_path(@user.id)
     else
@@ -17,9 +18,23 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
   end
 
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+     #binding.pry
+    if @user.update(user_params)
+      redirect_to user_path(@user.id), notice: "プロフィールを編集しました！"
+    else
+      render :edit
+    end
+  end
+
 
   private
   def user_params
-    params.require(:user).permit(:name, :email, :password, :password_confirmation, :image)
+    params.require(:user).permit(:name, :email, :password, :password_confirmation, :image, :image_cache)
   end
 end
