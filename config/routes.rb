a2bb06a5d82152869d1ef3d8d337fb39e09f71de
Yuta Hoshino <y.hoshino25@gmail.com>
2020-12-16 10:28:53 +0900

    お気に入り機能/簡易header作成

--- config/routes.rb
+++ config/routes.rb
@@ -6,5 +6,6 @@ Rails.application.routes.draw do
   end
   resources :sessions, only: [:new, :create, :destroy]
   resources :users
+  resources :favorites, only: [:create, :destroy]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
