d5cf7dad55fcce7d862ad6a15b3609dc7c23eb6d
Yuta Hoshino <y.hoshino25@gmail.com>
2020-12-23 11:53:24 +0900

    LetterOpenerの記述が本番環境に影響したため、修正

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -64,6 +64,8 @@ Rails.application.configure do
   # config.active_job.queue_name_prefix = "instagram_clone_#{Rails.env}"
 
   config.action_mailer.perform_caching = false
+  config.action_mailer.delivery_method = :letter_opener_web
+  config.action_mailer.default_url_options = { host: 'morning-oasis-88189.herokuapp.com/' }
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
